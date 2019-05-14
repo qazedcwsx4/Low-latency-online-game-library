f579a71e52780c26b540f65c97f7881587079c3b
qaze <qazedcwsx4@gmail.com>
2019-05-14 21:43:36 +0200

    Renames

--- ClientTCP.cpp
+++ ClientTCP.cpp
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "ClientTCP.h"
 
 
 void hello() {
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 //
 // Created by qaze on 14/05/2019.
 //
-#include "library.h"
+#include "ClientTCP.h"
 
 int main() {
     //hello();
     ClientTCP succ("127.0.0.1", 4200);
     succ.init();
-    succ.send("pedal");
+    succ.send("test");
     return 0;
 }
